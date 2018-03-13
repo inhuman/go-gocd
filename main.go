e55bb33e7b35495dbb132f89c736679c19ce310f
Ivan <msgexec@gmail.com>
2018-03-13 15:33:50 +0300

    added method for set custom http transport for superAgent

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"time"
 
 	"github.com/parnurzeal/gorequest"
+	"net/http"
+	"crypto/tls"
 )
 
 // DefaultClient entrypoint for GoCD
@@ -14,9 +16,21 @@ type DefaultClient struct {
 
 // New GoCD Client
 func New(host, username, password string) Client {
+
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			TLSHandshakeTimeout: 5 * time.Second,
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		}}
+
+	superAgent := gorequest.New()
+	superAgent.Client = httpClient
+
 	client := DefaultClient{
 		Host:    host,
-		Request: gorequest.New().Timeout(60*time.Second).SetBasicAuth(username, password),
+		Request: gorequest.New().Timeout(60 * time.Second).SetBasicAuth(username, password),
 	}
 	return &client
 }
@@ -24,4 +38,8 @@ func New(host, username, password string) Client {
 func (c *DefaultClient) resolve(resource string) string {
 	// TODO: Use a proper URL resolve to parse the string and append the resource
 	return c.Host + resource
+}
+
+func (c *DefaultClient) SetHttpClientTransport(transport http.Transport) {
+	c.Request.Client.Transport = &transport
 }
